b87db1d25238ef62f4e20bface9c6e7327149507
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-06-02 21:57:11 +0530

    Switch to nixos 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "Main NixOS Flake";
 
 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
 		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 		nixvim-config = {
 			url = "github:sujay1844/nixvim";
