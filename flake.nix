8156e66fd7d5e083b5d4623f5c75b6d750d16e4f
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-06-23 00:00:04 +0530

    Format indents

--- flake.nix
+++ flake.nix
@@ -1,41 +1,36 @@
 {
-	description = "Main NixOS Flake";
+  description = "Main NixOS Flake";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		nixvim-config = {
-			url = "github:sujay1844/nixvim";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim-config = { url = "github:sujay1844/nixvim"; };
+  };
 
-	outputs = inputs @ { self, nixpkgs, nixpkgs-unstable, nixvim-config, ... }:
-	let 
-		system = "x86_64-linux";
-		pkgs = import nixpkgs {
-			inherit system;
-			config = { allowUnfree = true; };
-		};
-		pkgs-unstable = import nixpkgs-unstable {
-			inherit system;
-			config = { allowUnfree = true; };
-		};
-	in {
-		nixosConfigurations = {
-			stormbreaker = nixpkgs.lib.nixosSystem {
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nixvim-config, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+      pkgs-unstable = import nixpkgs-unstable {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+    in {
+      nixosConfigurations = {
+        stormbreaker = nixpkgs.lib.nixosSystem {
 
-				specialArgs = {
-					inherit system;
-					inherit pkgs;
-					inherit pkgs-unstable;
-					inherit inputs;
-					inherit nixvim-config;
-				};
-				modules = [
-					./configuration.nix
-				];
-			};
-		};
-	};
+          specialArgs = {
+            inherit system;
+            inherit pkgs;
+            inherit pkgs-unstable;
+            inherit inputs;
+            inherit nixvim-config;
+          };
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
 }
-
